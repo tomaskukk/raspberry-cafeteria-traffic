617232830d36390b06146da576b0f40fa7c84fd0
tomaskukk <45310493+tomaskukk@users.noreply.github.com>
2020-01-21 11:41:09 +0200

    Removing unneeded code

--- capture.py
+++ capture.py
@@ -1,14 +1,10 @@
-import dropbox
-from dropbox.exceptions import ApiError, AuthError
 import time
 import datetime
 import picamera
 import sys, os
-import RPi.GPIO as GPIO
 import requests
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(23, GPIO.IN)
+
 
 
 PHOTOFORMAT = "jpeg"
@@ -28,35 +24,6 @@ def timestamp():
     return tstring.strftime("%Y%m%d_%H%M%S")
 
 def uploadFile(localfile):
-#    if (len(TOKEN) == 0):
-#        sys.exit("Error: Missing access token")
-#
-#    print ("creating db object")
-#    dbx = dropbox.Dropbox(TOKEN)
-#
-#    try:
-#        dbx.users_get_current_account()
-#    except AuthError as err:
-#        sys.exit("Error invalid access token")
-#
-#    uploadPath = "/" + localfile
-#
-#    with open(localfile, 'rb') as f:
-#        print("Uploading " + localfile + " to Dropbox as " + uploadPath + "...")
-#
-#        try:
-#            dbx.files_upload(f.read(), uploadPath)
-#        except ApiError as err:
-            # Check user has enough Dropbox space quota
-#            if (err.error.is_path() and
-#                   err.error.get_path().error.is_insufficient_space()):
-#                sys.exit("ERROR: Cannot upload; insufficient space.")
-#            elif err.user_message_text:
-#                print(err.user_message_text)
-#                sys.exit()
-#            else:
-#                print(err)
-#                sys.exit()
     # Send the image to remote backend
     url = "http://192.168.1.233:3001/api/traffic/picture"
     files = {'image': open(localfile, 'rb')}
@@ -65,7 +32,6 @@ def uploadFile(localfile):
         print("Picture send succesfully")
     else:
         print("Something went wrong")
-#    print(response)
 
 def deleteLocal(file):
     os.system("rm " + file)
@@ -89,14 +55,8 @@ def doAll():
     
 def main():
     while 1:
-#        if GPIO.input(23) == True:
-#            print ("Motion detected")
             doAll()
             time.sleep(60)
-#
-#       elif GPIO.input(23) == False:
-#            print ("No motion detected")
-#            time.sleep(1)
 
 if __name__ == "__main__":
     main()
